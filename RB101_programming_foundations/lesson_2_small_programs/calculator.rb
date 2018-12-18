4bcf736de6345d3eabfeaf41136d1c3d93d433fb
CristianCristea <cristiancristea.1987@gmail.com>
2018-12-18 21:13:25 +0100

    Refactor calculator #1

--- RB101_programming_foundations/lesson_2_small_programs/calculator.rb
+++ RB101_programming_foundations/lesson_2_small_programs/calculator.rb
@@ -4,11 +4,16 @@
 # asks for the type of operation to perform: add, subtract, multiply or divide
 # displays the result
 
-require 'pry'
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def valid_number?(num)
+  num.to_i != 0
+end
 
 def calculate(num1, num2, operator)
-  result = 0
-  result = case operator
+  case operator
   when '1'
     num1 + num2
   when '2'
@@ -16,17 +21,75 @@ def calculate(num1, num2, operator)
   when '3'
     num1 * num2
   when '4'
-    num2 == 0 ? 'Cannot divide by 0' : num1.to_f / num2.to_f
+    num1.to_f / num2.to_f
   end
-
-  result
 end
 
-puts "Fist number : "
-num1 = gets.chomp.to_i
-puts "Second number: "
-num2 = gets.chomp.to_i
-puts "Operation(1)add, 2)subtract, 3)multiply, 4)divide): "
-operator = gets.chomp
+def operation_to_message(operator)
+  case operator
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
 
-puts "Result: #{calculate(num1, num2, operator)}"
+prompt('Welcome to calculator."')
+loop do
+  num1 = ''
+  loop do
+    prompt('Fist number')
+    num1 = gets.chomp.to_i
+
+    if valid_number?(num1)
+      break
+    else
+      prompt('Not a valid number')
+    end
+  end
+
+  num2 = ''
+  loop do
+    prompt('Second number')
+    num2 = gets.chomp.to_i
+    if valid_number?(num2)
+      break
+    else
+      prompt('Not a valid number')
+    end
+  end
+
+  operatior_prompt = <<-MSG
+    Operation:
+    1) add
+    2) subtract
+    3) multiply
+    4) divide'
+  MSG
+
+  prompt(operatior_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if %w[1 2 3 4].include?(operator)
+      break
+    else
+      prompt('Not a valid operation, choose 1-4.')
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  prompt("Result: #{calculate(num1, num2, operator)}")
+  prompt('Do you want to perform another calcuation? (Y/N) ?')
+  answer = gets.chomp
+
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt('Goodbye!')
