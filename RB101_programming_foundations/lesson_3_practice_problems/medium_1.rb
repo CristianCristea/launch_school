5af8aad8a26eb05fd9fac58b879f559271e6e5cc
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-12-27 18:49:55 +0100

    Add medium 2 problems

--- RB101_programming_foundations/lesson_3_practice_problems/medium_1.rb
+++ RB101_programming_foundations/lesson_3_practice_problems/medium_1.rb
@@ -54,3 +54,140 @@ end
 
 
 # 4.
+# Alyssa was asked to write an implementation of a rolling buffer. Elements are added to the rolling buffer and if the buffer becomes full, then new elements that are added will displace the oldest elements in the buffer.
+
+# She wrote two implementations saying, "Take your pick. Do you like << or + for modifying the buffer?". Is there a difference between the two, other than what operator she chose to use to add an element to the buffer?
+
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+# first method will mutate the caller
+# second will not
+
+# 5.
+# Alyssa asked Ben to write up a basic implementation of a Fibonacci calculator, A user passes in two numbers, and the calculator will keep computing the sequence until some limit is reached.
+
+# Ben coded up this implementation but complained that as soon as he ran it, he got an error. Something about the limit variable. What's wrong with the code?
+
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
+# How would you fix this so that it works?
+# local variable scope: the limit variable is not available inside the method definition
+# fix: define another parameter in the method definition and pass limit as an argument at method invocation
+
+
+# 6.
+# In an earlier practice problem we saw that depending on variables to be modified by called methods can be tricky:
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+# We learned that whether the above "coincidentally" does what we think we wanted "depends" upon what is going on inside the method.
+
+# How can we refactor this practice problem to make the result easier to predict and easier for the next programmer to maintain?
+
+# depends on what the method should do
+# if it should mutate the caller and return the new data
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+
+  return a_string_param, an_array_param # returns an array
+end
+
+# 7.
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8 # => 34
+
+# 8.
+# One day Spot was playing with the Munster family's home computer and he wrote a small program to mess with their demographic data:
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+# After writing this method, he typed the following...and before Grandpa could stop him, he hit the Enter key with his tail:
+
+mess_with_demographics(munsters)
+# Did the family's data get ransacked? Why or why not?
+# yes the caller was mutated
+# the variable passed to the method invocation is a pointer to the muster object
+
+# 9.
+# Method calls can take expressions as arguments. Suppose we define a method called rps as follows, which follows the classic rules of rock-paper-scissors game, but with a slight twist that it declares whatever hand was used in the tie as the result of that tie.
+
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+# What is the result of the following call?
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
+# "paper"
+
+# 10.
+# Consider these two simple methods:
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+# What would be the return value of the following method invocation?
+
+bar(foo)
+
+# "no"
