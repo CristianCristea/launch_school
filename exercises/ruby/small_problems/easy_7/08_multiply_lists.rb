a7c38351631612460b127131cce16c65bd23196c
Cristian Cristea <cristiancristea.1987@gmail.com>
2019-02-27 10:30:55 +0100

    Add exercises

---
+++ exercises/ruby/small_problems/easy_7/08_multiply_lists.rb
@@ -0,0 +1,16 @@
+# Multiply Lists
+# Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
+
+def multiply_list(arr1, arr2)
+  arr1.map.with_index do |num, index|
+    num * arr2[index]
+  end
+end
+
+# using zip
+def multiply_list(arr1, arr2)
+  arr1.zip(arr2).map {|arr| arr.reduce(:*) }
+end
+
+# Examples:
+p multiply_list([3, 5, 7], [9, 10, 11])# == [27, 50, 77]
