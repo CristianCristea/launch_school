b809213e3f292460a525220bdf76f6784aa445e8
CristianCristea <cristiancristea.1987@gmail.com>
2019-03-03 11:51:29 +0100

    Add exercises

--- exercises/ruby/small_problems/medium_1/02_rotate_2.rb
+++ exercises/ruby/small_problems/medium_1/02_rotate_2.rb
@@ -1,18 +1,24 @@
-Rotation (Part 2)
-Write a method that can rotate the last n digits of a number. For example:
-Note that rotating just 1 digit results in the original number being returned.
+# Rotation (Part 2)
+# Write a method that can rotate the last n digits of a number. For example:
+# Note that rotating just 1 digit results in the original number being returned.
 
-You may use the rotate_array method from the previous exercise if you want. (Recommended!)
+# You may use the rotate_array method from the previous exercise if you want. (Recommended!)
 
-You may assume that n is always a positive integer.
+# You may assume that n is always a positive integer.
 
-def rotate_rightmost_digits(num, digits)
+# convert to string, to array
+# 
 
+def rotate_rightmost_digits(number, digit)
+  numbers = number.to_s.chars
+  numbers << numbers.slice!(-digit)
+
+  numbers.join('').to_i
 end
 
-rotate_rightmost_digits(735291, 1) == 735291
-rotate_rightmost_digits(735291, 2) == 735219
-rotate_rightmost_digits(735291, 3) == 735912
-rotate_rightmost_digits(735291, 4) == 732915
-rotate_rightmost_digits(735291, 5) == 752913
-rotate_rightmost_digits(735291, 6) == 352917
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
